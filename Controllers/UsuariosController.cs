6d7540d0efedf954912d602a3ba563763849ad96
carlos garcía mena <karlogm7@hotmail.com>
2022-02-13 11:17:54 -0600

    ok1

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,21 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Usuarios.Controllers
+{
+    [Route("/Nombre")]
+    public class UsuariosController : Controller
+    {
+        [Route("/Nombre/Alex")]
+        //[HttpGet]
+        //[Route("/Usuarios/Alex")] //Nueva ruta para esta pagiona/ no se ejecuta hasta que se escriba en el navegador de 5 a 9
+        public IActionResult Index(int id )
+        {
+            //ViewData["iD"] = id;
+            return View();
+        }
+    }
+}
